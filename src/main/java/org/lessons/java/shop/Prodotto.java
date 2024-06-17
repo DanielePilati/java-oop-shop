ef96b2cc0a2e0c91d107c8a06a34c398252eb8e6
DanielePilati <165797370+DanielePilati@users.noreply.github.com>
2024-06-17 19:34:22 +0200

    add getter and setter

--- src/main/java/org/lessons/java/shop/Prodotto.java
+++ src/main/java/org/lessons/java/shop/Prodotto.java
@@ -2,4 +2,49 @@ package org.lessons.java.shop;
 
 public class Prodotto {
 
+
+	private int maxCode=999999;
+	private int minCode=0;
+	private int codice;
+	private String nome;
+	private String descrizione;
+	private double prezzo;
+	private int iva;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescrizione() {
+		return descrizione;
+	}
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+	public double getPrezzo() {
+		return prezzo;
+	}
+	public void setPrezzo(double prezzo) {
+		this.prezzo = prezzo;
+	}
+	public int getIva() {
+		return iva;
+	}
+	public void setIva(int iva) {
+		this.iva = iva;
+	}
+	public Prodotto() {
+		this.codice = (int) (Math.random()* (maxCode - minCode + 1)) + minCode;
+	}
+	public int getCodice() {
+		return this.codice;
+	}
+
+	
+	
+	
+	
+	
 }
